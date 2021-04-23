623d5299186c06d75336c8683eb146fb12f0500d
JineshMajithia <mjinesh24@gmail.com>
2021-04-23 16:24:01 +0530

    Changes done !

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+export default function App() {
+  const [likes, setLikes] = useState(100);
+  const [dislikes, setDislikes] = useState(25);
+
+  const [liked, setLiked] = useState(false);
+  const [disliked, setDisliked] = useState(false);
+
+  const likeHandler = () => {
+    if (disliked) {
+      setDisliked(false);
+      setDislikes(dislikes - 1);
+    }
+    if (liked) {
+      setLiked(false);
+      setLikes(likes - 1);
+    } else {
+      setLiked(true);
+      setLikes(likes + 1);
+    }
+  };
+
+  const dislikeHandler = () => {
+    if (liked) {
+      setLiked(false);
+      setLikes(likes - 1);
+    }
+    if (disliked) {
+      setDisliked(false);
+      setDislikes(dislikes - 1);
+    } else {
+      setDisliked(true);
+      setDislikes(dislikes + 1);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button
+        className={liked ? "like-button liked" : "like-button"}
+        onClick={likeHandler}
+      >
+        Like | <span className="likes-counter">{likes}</span>
+      </button>
+      <button
+        className={disliked ? "dislike-button disliked" : "dislike-button"}
+        onClick={dislikeHandler}
+      >
+        Dislike | <span className="dislikes-counter">{dislikes}</span>
+      </button>
     </div>
   );
 }
-
-export default App;
